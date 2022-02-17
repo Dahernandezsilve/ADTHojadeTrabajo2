86c2e1397b422313dff3fbfae15dc55d3cbe3985
Diego Hernández <dahernandezsilvestre@gmail.com>
2022-02-16 19:25:56 -0600

    Añadir interfaz al archivo.

---
+++ HojaDeTrabajo2/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        Scanner s = new Scanner(System.in);
+        int seleccion;
+        boolean salir = false;
+        while (!salir){
+            System.out.println();
+
+        }
+    }
+}
